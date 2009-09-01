6c4a304df4462c84b372ddaf34159628fcccbb27
Bruce Wedan <brucewedan@gmail.com>
2009-09-01 21:34:06 +0000

    script updates
    git-svn-id: http://svn.code.sf.net/p/cgns/code/cgns/trunk@108 59ee3124-ab72-4556-8a47-5b88dfbcc004

--- src/cgnstools/cgnscalc/cgnscalc.bat
+++ src/cgnstools/cgnscalc/cgnscalc.bat
@@ -1,13 +1,11 @@
 @echo off
 setlocal
 
-rem may need to set these if system can't find Tcl and TK scripts
-rem set TCL_LIBRARY_PATH=c:/progra~1/tcl/lib/tcl8.3
-rem set TK_LIBRARY_PATH=c:/progra~1/tcl/lib/tk8.3
-
 set VIEWERDIR=%~dps0
 if not exist %VIEWERDIR%calcwish.exe goto notfound
 if not exist %VIEWERDIR%cgnscalc.tcl goto notfound
+
+call %VIEWERDIR%..\cgconfig.bat
 
 start /b %VIEWERDIR%calcwish.exe %VIEWERDIR%cgnscalc.tcl %1
 goto done
