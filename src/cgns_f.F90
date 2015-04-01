4532ae2cbdb5b75601786899fbc2450771108a15
M. Scot Breitenfeld <brtnfld@hdfgroup.org>
2015-04-01 13:22:57 -0500

    added code disclaimer

--- src/cgns_f.F90
+++ src/cgns_f.F90
@@ -1,3 +1,39 @@
+!-------------------------------------------------------------------------
+!This software is provided 'as-is', without any express or implied warranty.
+!In no event will the authors be held liable for any damages arising from
+!the use of this software.
+!
+!Permission is granted to anyone to use this software for any purpose,
+!including commercial applications, and to alter it and redistribute it
+!freely, subject to the following restrictions:
+!
+!1. The origin of this software must not be misrepresented; you must not
+!   claim that you wrote the original software. If you use this software
+!   in a product, an acknowledgment in the product documentation would be
+!   appreciated but is not required.
+!
+!2. Altered source versions must be plainly marked as such, and must not
+!   be misrepresented as being the original software.
+!
+!3. This notice may not be removed or altered from any source distribution.
+!-------------------------------------------------------------------------
+!   _____ _____ _   _  _____ 
+!  / ____/ ____| \ | |/ ____|
+! | |   | |  __|  \| | (___  
+! | |   | | |_ | . ` |\___ \ 
+! | |___| |__| | |\  |____) |
+!  \_____\_____|_| \_|_____/ 
+!                            
+!  PURPOSE:
+!    Provides a module for the Fortran wrapper interfaces and CGNS
+!    constant parameters.
+!
+!
+!  KNOW ISSUES:
+!    Routines passing an argument to a C API argument of type
+!    void * do not have explicit interfaces. They are the routines which 
+!    are commented out.
+!                        
 MODULE cgns
 
   USE ISO_C_BINDING
