24b46abe058221642f7ea3cf76bf2cd57722c20e
M. Scot Breitenfeld <brtnfld@hdfgroup.org>
2015-09-22 16:37:37 -0500

    changed to using 1.8 hdf branch.

--- install-hdf.sh
+++ install-hdf.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
-svn co https://svn.hdfgroup.uiuc.edu/hdf5/trunk
-cd trunk && ./autogen.sh ./configure --disable-fortran --disable-hl CC=gcc && make && make install
+
+#trunk
+#svn co https://svn.hdfgroup.uiuc.edu/hdf5/trunk
+#cd trunk && ./autogen.sh ./configure --disable-fortran --disable-hl CC=gcc && make && make install
+
+# 1.8 branch
+svn co https://svn.hdfgroup.uiuc.edu/hdf5/branches/hdf5_1_8
+cd trunk && ./configure --disable-fortran --disable-hl CC=gcc && make && make install
