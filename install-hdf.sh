96da6a5ec1236b4006b9e485f16dc5082b5f60fc
M. Scot Breitenfeld <brtnfld@hdfgroup.org>
2015-09-22 16:28:25 -0500

    changed shell for script.

--- install-hdf.sh
+++ install-hdf.sh
@@ -1,4 +1,3 @@
-#!/bin/csh
+#!/bin/sh
 svn co https://svn.hdfgroup.uiuc.edu/hdf5/trunk
-setenv CC gcc
-cd trunk && ./autogen.sh ./configure --disable-fortran --disable-hl && make && make install
+cd trunk && ./autogen.sh ./configure --disable-fortran --disable-hl CC=gcc && make && make install
