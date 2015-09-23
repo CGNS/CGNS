5a6298bea7e96c6a795aa03ecc9b305607d8753e
M. Scot Breitenfeld <brtnfld@hdfgroup.org>
2015-09-23 15:11:16 -0500

    single branch checkout of CGNS

--- install-cgns.sh
+++ install-cgns.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
-git clone https://github.com/CGNS/CGNS.git
+git clone -b develop --single-branch https://github.com/CGNS/CGNS.git
 cd CGNS/src
-git checkout develop
 export FC=gfortran
 export F77=gfortran
 export CC=gcc
 export FLIBS="-Wl,--no-as-needed -ldl -lz"
-export LDFLAGS="-Wl,--no-as-needed -ldl"
 export LIBS="-Wl,--no-as-needed -ldl -lz"
 ./configure \
 --with-hdf5=$HOME/hdf5 \
