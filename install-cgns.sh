677007628000ae17d45d80ce09d931a181bf6cf5
M. Scot Breitenfeld <brtnfld@hdfgroup.org>
2015-10-16 12:15:21 -0500

    testing

--- install-cgns.sh
+++ install-cgns.sh
@@ -3,9 +3,15 @@ set -e
 #git clone -b develop --single-branch https://github.com/CGNS/CGNS.git
 #cd CGNS/src
 cd src
-export FC=gfortran
-export F77=gfortran
-export CC=gcc
+if [[ $TRAVIS_OS_NAME == "osx" ]]; then
+  export FC=gfortran-4
+  export F77=gfortran-4
+  export CC=gcc-4
+else
+  export FC=gfortran
+  export F77=gfortran
+  export CC=gcc
+fi
 export FLIBS="-Wl,--no-as-needed -ldl -lz"
 export LIBS="-Wl,--no-as-needed -ldl -lz"
 ./configure \
--- install-hdf.sh
+++ install-hdf.sh
@@ -3,7 +3,11 @@
 #trunk
 #svn co https://svn.hdfgroup.uiuc.edu/hdf5/trunk
 #cd trunk && ./autogen.sh ./configure --disable-fortran --disable-hl CC=gcc && make && make install
-
+if [[ $TRAVIS_OS_NAME == "osx" ]]; then
+  export CC=gcc-4
+else
+  export CC=gcc
+fi
 # 1.8 branch
 svn co -q https://svn.hdfgroup.uiuc.edu/hdf5/branches/hdf5_1_8
-cd hdf5_1_8 && ./configure --disable-fortran --disable-hl --prefix=$HOME/hdf5 CC=gcc && make > result.txt 2>&1 && make install
+cd hdf5_1_8 && ./configure --disable-fortran --disable-hl --prefix=$HOME/hdf5 && make > result.txt 2>&1 && make install
