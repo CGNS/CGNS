462efec1c75c22ab301b4dbbb3a236d550a4063b
M. Scot Breitenfeld <brtnfld@hdfgroup.org>
2015-10-16 13:53:20 -0500

    testing2

--- install-hdf.sh
+++ install-hdf.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-echo "****@@"
-find / -iname 'gfortran*'
-echo "****@@"
 #trunk
 #svn co https://svn.hdfgroup.uiuc.edu/hdf5/trunk
 #cd trunk && ./autogen.sh ./configure --disable-fortran --disable-hl CC=gcc && make && make install
