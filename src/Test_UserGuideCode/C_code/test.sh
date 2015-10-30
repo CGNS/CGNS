fc6ae0bb3909982220b85c13c2d1c7f82ed4fd17
M. Scot Breitenfeld <brtnfld@hdfgroup.org>
2015-10-30 14:27:50 -0500

    Remove the use of the -I option in diff.

--- src/Test_UserGuideCode/C_code/test.sh
+++ src/Test_UserGuideCode/C_code/test.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#! /bin/bash
 NO_COLOR="\033[0m"
 OK_COLOR="\033[32;01m"
 ERROR_COLOR="\033[31;01m"
@@ -8,7 +8,7 @@ echoresults() {
     then
         printf "$ERROR_COLOR *** FAILED *** $NO_COLOR \n"
     else
-        printf "$OK_COLOR PASSED $NO_COLOR \n"
+        printf "$OK_COLOR passed $NO_COLOR \n"
     fi
 }
 
@@ -26,49 +26,49 @@ cd $dir/build
 
 ./write_grid_str
 ./read_grid_str > output1
-diff -I 'Library Version used for file creation*' output1 ../OUTPUT1 > results1.txt
+diff <( sed '/Library/ d' output1) <( sed '/Library/ d' ../OUTPUT1) > results1.txt
 status=$?
 echoresults $status
 return_val=`expr $status + $return_val`
 
 ./write_flowvert_str
 ./read_flowvert_str > output2
-diff -I 'Library Version used for file creation*' output2 ../OUTPUT2 > results2.txt
+diff <( sed '/Library/ d' output2) <( sed '/Library/ d' ../OUTPUT2) > results2.txt
 status=$?
 echoresults $status
 return_val=`expr $status + $return_val`
 
 ./write_nondimensional
 ./read_nondimensional > output3
-diff -I 'Library Version used for file creation*' output3 ../OUTPUT3 > results3.txt
+diff <( sed '/Library/ d' output3) <( sed '/Library/ d' ../OUTPUT3) > results3.txt
 status=$?
 echoresults $status
 return_val=`expr $status + $return_val`
 
 ./write_descriptor
 ./read_descriptor > output4
-diff -I 'Library Version used for file creation*' output4 ../OUTPUT4 > results4.txt
+diff <( sed '/Library/ d' output4) <( sed '/Library/ d' ../OUTPUT4) > results4.txt
 status=$?
 echoresults $status
 return_val=`expr $status + $return_val`
 
 ./write_convergence
 ./read_convergence > output5
-diff -I 'Library Version used for file creation*' output5 ../OUTPUT5 > results5.txt
+diff <( sed '/Library/ d' output5) <( sed '/Library/ d' ../OUTPUT5) > results5.txt
 status=$?
 echoresults $status
 return_val=`expr $status + $return_val`
 
 ./write_floweqn_str
 ./read_floweqn_str > output6
-diff -I 'Library Version used for file creation*' output6 ../OUTPUT6 > results6.txt
+diff <( sed '/Library/ d' output6) <( sed '/Library/ d' ../OUTPUT6) > results6.txt
 status=$?
 echoresults $status
 return_val=`expr $status + $return_val`
 
 ./write_bcpnts_str
 ./read_bcpnts_str > output7
-diff -I 'Library Version used for file creation*' output7 ../OUTPUT7 > results6.txt
+diff <( sed '/Library/ d' output7) <( sed '/Library/ d' ../OUTPUT7) > results7.txt
 status=$?
 echoresults $status
 return_val=`expr $status + $return_val`
@@ -83,7 +83,7 @@ cd $dir/build
 
 ./write_grid_unst
 ./read_grid_unst > output1
-diff -I 'Library Version used for file creation*' output1 ../OUTPUT1 > results1.txt
+diff <( sed '/Library/ d' output1) <( sed '/Library/ d' ../OUTPUT1) > results1.txt
 status=$?
 echoresults $status
 return_val=`expr $status + $return_val`
@@ -97,28 +97,28 @@ return_val=`expr $status + $return_val`
 
 ./write_dimensional
 ./read_dimensional > output3
-diff -I 'Library Version used for file creation*' output3 ../OUTPUT3 > results3.txt
+diff <( sed '/Library/ d' output3) <( sed '/Library/ d' ../OUTPUT3) > results3.txt
 status=$?
 echoresults $status
 return_val=`expr $status + $return_val`
 
 ./write_descriptor
 ./read_descriptor > output4
-diff -I 'Library Version used for file creation*' output4 ../OUTPUT4 > results4.txt
+diff <( sed '/Library/ d' output4) <( sed '/Library/ d' ../OUTPUT4) > results4.txt
 status=$?
 echoresults $status
 return_val=`expr $status + $return_val`
 
 ./write_convergence
 ./read_convergence > output5
-diff -I 'Library Version used for file creation*' output5 ../OUTPUT5 > results5.txt
+diff <( sed '/Library/ d' output5) <( sed '/Library/ d' ../OUTPUT5) > results5.txt
 status=$?
 echoresults $status
 return_val=`expr $status + $return_val`
 
 ./write_bcpnts_unst
 ./read_bcpnts_unst > output6
-diff -I 'Library Version used for file creation*' output6 ../OUTPUT6 > results6.txt
+diff <( sed '/Library/ d' output6) <( sed '/Library/ d' ../OUTPUT6) > results6.txt
 status=$?
 echoresults $status
 return_val=`expr $status + $return_val`
@@ -133,14 +133,14 @@ cd $dir/build
 
 ./write_grid_str
 ./read_grid_str > output1
-diff -I 'Library Version used for file creation*' output1 ../OUTPUT1 > results1.txt
+diff <( sed '/Library/ d' output1) <( sed '/Library/ d' ../OUTPUT1) > results1.txt
 status=$?
 echoresults $status
 return_val=`expr $status + $return_val`
 
 ./write_timevert_str
 ./read_timevert_str > output2
-diff -I 'Library Version used for file creation*' output2 ../OUTPUT2 > results2.txt
+diff <( sed '/Library/ d' output2) <( sed '/Library/ d' ../OUTPUT2) > results2.txt
 status=$?
 echoresults $status
 return_val=`expr $status + $return_val`
@@ -155,21 +155,21 @@ cd $dir/build
 
 ./write_grid_str
 ./read_grid_str > output1
-diff -I 'Library Version used for file creation*' output1 ../OUTPUT1 > results1.txt
+diff <( sed '/Library/ d' output1) <( sed '/Library/ d' ../OUTPUT1) > results1.txt
 status=$?
 echoresults $status
 return_val=`expr $status + $return_val`
 
 ./write_flowcent_str
 ./read_flowcent_str > output2
-diff -I 'Library Version used for file creation*' output2 ../OUTPUT2 > results2.txt
+diff <( sed '/Library/ d' output2) <( sed '/Library/ d' ../OUTPUT2) > results2.txt
 status=$?
 echoresults $status
 return_val=`expr $status + $return_val`
 
 ./write_bc_str
 ./read_bc_str > output3
-diff -I 'Library Version used for file creation*' output3 ../OUTPUT3 > results3.txt
+diff <( sed '/Library/ d' output3) <( sed '/Library/ d' ../OUTPUT3) > results3.txt
 status=$?
 echoresults $status
 return_val=`expr $status + $return_val`
@@ -184,14 +184,14 @@ cd $dir/build
 
 ./write_grid_str
 ./read_grid_str > output1
-diff -I 'Library Version used for file creation*' output1 ../OUTPUT1 > results1.txt
+diff <( sed '/Library/ d' output1) <( sed '/Library/ d' ../OUTPUT1) > results1.txt
 status=$?
 echoresults $status
 return_val=`expr $status + $return_val`
 
 ./write_flowcentrind_str
 ./read_flowcentrind_str > output2
-diff -I 'Library Version used for file creation*' output2 ../OUTPUT2 > results2.txt
+diff <( sed '/Library/ d' output2) <( sed '/Library/ d' ../OUTPUT2) > results2.txt
 status=$?
 echoresults $status
 return_val=`expr $status + $return_val`
@@ -206,14 +206,14 @@ cd $dir/build
 
 ./write_grid2zn_str
 ./read_grid2zn_str > output1
-diff -I 'Library Version used for file creation*' output1 ../OUTPUT1 > results1.txt
+diff <( sed '/Library/ d' output1) <( sed '/Library/ d' ../OUTPUT1) > results1.txt
 status=$?
 echoresults $status
 return_val=`expr $status + $return_val`
 
 ./write_con2zn_str
 ./read_con2zn_str > output2
-diff -I 'Library Version used for file creation*' output2 ../OUTPUT2 > results2.txt
+diff <( sed '/Library/ d' output2) <( sed '/Library/ d' ../OUTPUT2) > results2.txt
 status=$?
 echoresults $status
 return_val=`expr $status + $return_val`
@@ -228,14 +228,14 @@ cd $dir/build
 
 ./write_grid2zn_str
 ./read_grid2zn_str > output1
-diff -I 'Library Version used for file creation*' output1 ../OUTPUT1 > results1.txt
+diff <( sed '/Library/ d' output1) <( sed '/Library/ d' ../OUTPUT1) > results1.txt
 status=$?
 echoresults $status
 return_val=`expr $status + $return_val`
 
 ./write_con2zn_genrl_str
 ./read_con2zn_genrl_str > output2
-diff -I 'Library Version used for file creation*' output2 ../OUTPUT2 > results2.txt
+diff <( sed '/Library/ d' output2) <( sed '/Library/ d' ../OUTPUT2) > results2.txt
 status=$?
 echoresults $status
 return_val=`expr $status + $return_val`
