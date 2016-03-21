cc91ceb310f35e7d9bd240b9a87a1d9edc1151c1
M. Scot Breitenfeld <brtnfld@hdfgroup.org>
2016-03-21 10:31:28 -0500

    added loop for tests

--- src/Test_UserGuideCode/C_code/test.sh
+++ src/Test_UserGuideCode/C_code/test.sh
@@ -20,225 +20,164 @@ return_val=0
 # Special cases
 ###############################
 
+## declare an array of tests
+declare -a w_arr=("write_grid_str" "write_flowvert_str" "write_nondimensional" "write_descriptor" \
+	"write_convergence" "write_floweqn_str" "write_bcpnts_str")
+declare -a r_arr=("read_grid_str" "read_flowvert_str" "read_nondimensional" "read_descriptor" \
+	"read_convergence" "read_floweqn_str" "read_bcpnts_str")
+
 dir=Test_Grid_Str
 printf "%-40s" "Testing $dir..."
 cd $dir/build
 
-./write_grid_str
-./read_grid_str > output1
-diff <( sed '/Library/ d' output1) <( sed '/Library/ d' ../OUTPUT1) > results1.txt
-status=$?
-echoresults $status
-return_val=`expr $status + $return_val`
-
-./write_flowvert_str
-./read_flowvert_str > output2
-diff <( sed '/Library/ d' output2) <( sed '/Library/ d' ../OUTPUT2) > results2.txt
-status=$?
-echoresults $status
-return_val=`expr $status + $return_val`
-
-./write_nondimensional
-./read_nondimensional > output3
-diff <( sed '/Library/ d' output3) <( sed '/Library/ d' ../OUTPUT3) > results3.txt
-status=$?
-echoresults $status
-return_val=`expr $status + $return_val`
-
-./write_descriptor
-./read_descriptor > output4
-diff <( sed '/Library/ d' output4) <( sed '/Library/ d' ../OUTPUT4) > results4.txt
-status=$?
-echoresults $status
-return_val=`expr $status + $return_val`
-
-./write_convergence
-./read_convergence > output5
-diff <( sed '/Library/ d' output5) <( sed '/Library/ d' ../OUTPUT5) > results5.txt
-status=$?
-echoresults $status
-return_val=`expr $status + $return_val`
-
-./write_floweqn_str
-./read_floweqn_str > output6
-diff <( sed '/Library/ d' output6) <( sed '/Library/ d' ../OUTPUT6) > results6.txt
-status=$?
-echoresults $status
-return_val=`expr $status + $return_val`
-
-./write_bcpnts_str
-./read_bcpnts_str > output7
-diff <( sed '/Library/ d' output7) <( sed '/Library/ d' ../OUTPUT7) > results7.txt
-status=$?
-echoresults $status
-return_val=`expr $status + $return_val`
+# loop through tests
+size_arr=${#w_arr[@]} #Number of elements in the array
+for i in $(seq 1 $size_arr);do
+   ./${w_arr[$i-1]}
+   ./${r_arr[$i-1]} > output$i
+   diff <( sed '/Library/ d' output$i) <( sed '/Library/ d' ../OUTPUT$i) > results$i.txt
+   status=$?
+   echoresults $status
+   return_val=`expr $status + $return_val`
+done
 
 cd ../..
 
 ###############################
+
+## declare an array of tests
+declare -a w_arr=("write_grid_unst" "write_flowvert_unst" "write_dimensional" "write_descriptor" "write_convergence" "write_bcpnts_unst")
+declare -a r_arr=("read_grid_unst" "read_flowvert_unst" "read_dimensional" "read_descriptor" "read_convergence" "read_bcpnts_unst")
 
 dir=Test_Grid_Unstr
 printf "%-40s" "Testing $dir..."
 cd $dir/build
 
-./write_grid_unst
-./read_grid_unst > output1
-diff <( sed '/Library/ d' output1) <( sed '/Library/ d' ../OUTPUT1) > results1.txt
-status=$?
-echoresults $status
-return_val=`expr $status + $return_val`
-
-./write_flowvert_unst
-./read_flowvert_unst > output2
-diff <( sed '/Library/ d' output2) <( sed '/Library/ d' ../OUTPUT2) > results2.txt
-status=$?
-echoresults $status
-return_val=`expr $status + $return_val`
-
-./write_dimensional
-./read_dimensional > output3
-diff <( sed '/Library/ d' output3) <( sed '/Library/ d' ../OUTPUT3) > results3.txt
-status=$?
-echoresults $status
-return_val=`expr $status + $return_val`
-
-./write_descriptor
-./read_descriptor > output4
-diff <( sed '/Library/ d' output4) <( sed '/Library/ d' ../OUTPUT4) > results4.txt
-status=$?
-echoresults $status
-return_val=`expr $status + $return_val`
-
-./write_convergence
-./read_convergence > output5
-diff <( sed '/Library/ d' output5) <( sed '/Library/ d' ../OUTPUT5) > results5.txt
-status=$?
-echoresults $status
-return_val=`expr $status + $return_val`
-
-./write_bcpnts_unst
-./read_bcpnts_unst > output6
-diff <( sed '/Library/ d' output6) <( sed '/Library/ d' ../OUTPUT6) > results6.txt
-status=$?
-echoresults $status
-return_val=`expr $status + $return_val`
+# loop through tests
+size_arr=${#w_arr[@]} #Number of elements in the array
+for i in $(seq 1 $size_arr);do
+   ./${w_arr[$i-1]}
+   ./${r_arr[$i-1]} > output$i
+   diff <( sed '/Library/ d' output$i) <( sed '/Library/ d' ../OUTPUT$i) > results$i.txt
+   status=$?
+   echoresults $status
+   return_val=`expr $status + $return_val`
+done
 
 cd ../..
 
 ###############################
+
+## declare an array of tests
+declare -a w_arr=("write_grid_str" "write_timevert_str")
+declare -a r_arr=("read_grid_str" "read_timevert_str")
 
 dir=Test_Grid_Str_Timeacc
 printf "%-40s" "Testing $dir..."
 cd $dir/build
 
-./write_grid_str
-./read_grid_str > output1
-diff <( sed '/Library/ d' output1) <( sed '/Library/ d' ../OUTPUT1) > results1.txt
-status=$?
-echoresults $status
-return_val=`expr $status + $return_val`
-
-./write_timevert_str
-./read_timevert_str > output2
-diff <( sed '/Library/ d' output2) <( sed '/Library/ d' ../OUTPUT2) > results2.txt
-status=$?
-echoresults $status
-return_val=`expr $status + $return_val`
+# loop through tests
+size_arr=${#w_arr[@]} #Number of elements in the array
+for i in $(seq 1 $size_arr);do
+   ./${w_arr[$i-1]}
+   ./${r_arr[$i-1]} > output$i
+   diff <( sed '/Library/ d' output$i) <( sed '/Library/ d' ../OUTPUT$i) > results$i.txt
+   status=$?
+   echoresults $status
+   return_val=`expr $status + $return_val`
+done
 
 cd ../..
 
 ###############################
+
+## declare an array of tests
+declare -a w_arr=("write_grid_str" "write_flowcent_str" "write_bc_str")
+declare -a r_arr=("read_grid_str" "read_flowcent_str" "read_bc_str")
 
 dir=Test_Grid_Str_FlowCent
 printf "%-40s" "Testing $dir..."
 cd $dir/build
 
-./write_grid_str
-./read_grid_str > output1
-diff <( sed '/Library/ d' output1) <( sed '/Library/ d' ../OUTPUT1) > results1.txt
-status=$?
-echoresults $status
-return_val=`expr $status + $return_val`
-
-./write_flowcent_str
-./read_flowcent_str > output2
-diff <( sed '/Library/ d' output2) <( sed '/Library/ d' ../OUTPUT2) > results2.txt
-status=$?
-echoresults $status
-return_val=`expr $status + $return_val`
-
-./write_bc_str
-./read_bc_str > output3
-diff <( sed '/Library/ d' output3) <( sed '/Library/ d' ../OUTPUT3) > results3.txt
-status=$?
-echoresults $status
-return_val=`expr $status + $return_val`
+# loop through tests
+size_arr=${#w_arr[@]} #Number of elements in the array
+for i in $(seq 1 $size_arr);do
+   ./${w_arr[$i-1]}
+   ./${r_arr[$i-1]} > output$i
+   diff <( sed '/Library/ d' output$i) <( sed '/Library/ d' ../OUTPUT$i) > results$i.txt
+   status=$?
+   echoresults $status
+   return_val=`expr $status + $return_val`
+done
 
 cd ../..
 
 ###############################
+
+## declare an array of tests
+declare -a w_arr=("write_grid_str" "write_flowcentrind_str")
+declare -a r_arr=("read_grid_str" "read_flowcentrind_str")
 
 dir=Test_Grid_Str_FlowCentRind
 printf "%-40s" "Testing $dir..."
 cd $dir/build
 
-./write_grid_str
-./read_grid_str > output1
-diff <( sed '/Library/ d' output1) <( sed '/Library/ d' ../OUTPUT1) > results1.txt
-status=$?
-echoresults $status
-return_val=`expr $status + $return_val`
-
-./write_flowcentrind_str
-./read_flowcentrind_str > output2
-diff <( sed '/Library/ d' output2) <( sed '/Library/ d' ../OUTPUT2) > results2.txt
-status=$?
-echoresults $status
-return_val=`expr $status + $return_val`
+# loop through tests
+size_arr=${#w_arr[@]} #Number of elements in the array
+for i in $(seq 1 $size_arr);do
+   ./${w_arr[$i-1]}
+   ./${r_arr[$i-1]} > output$i
+   diff <( sed '/Library/ d' output$i) <( sed '/Library/ d' ../OUTPUT$i) > results$i.txt
+   status=$?
+   echoresults $status
+   return_val=`expr $status + $return_val`
+done
 
 cd ../..
 
 ###############################
+
+## declare an array of tests
+declare -a w_arr=("write_grid2zn_str" "write_con2zn_str")
+declare -a r_arr=("read_grid2zn_str" "read_con2zn_str")
 
 dir=Test_Grid_Str_2zn
 printf "%-40s" "Testing $dir..."
 cd $dir/build
 
-./write_grid2zn_str
-./read_grid2zn_str > output1
-diff <( sed '/Library/ d' output1) <( sed '/Library/ d' ../OUTPUT1) > results1.txt
-status=$?
-echoresults $status
-return_val=`expr $status + $return_val`
-
-./write_con2zn_str
-./read_con2zn_str > output2
-diff <( sed '/Library/ d' output2) <( sed '/Library/ d' ../OUTPUT2) > results2.txt
-status=$?
-echoresults $status
-return_val=`expr $status + $return_val`
+# loop through tests
+size_arr=${#w_arr[@]} #Number of elements in the array
+for i in $(seq 1 $size_arr);do
+   ./${w_arr[$i-1]}
+   ./${r_arr[$i-1]} > output$i
+   diff <( sed '/Library/ d' output$i) <( sed '/Library/ d' ../OUTPUT$i) > results$i.txt
+   status=$?
+   echoresults $status
+   return_val=`expr $status + $return_val`
+done
 
 cd ../..
 
 ###############################
 
+## declare an array of tests
+declare -a w_arr=("write_grid2zn_str" "write_con2zn_genrl_str")
+declare -a r_arr=("read_grid2zn_str" "read_con2zn_genrl_str")
+
 dir=Test_Grid_Str_2zngenrl
 printf "%-40s" "Testing $dir..."
 cd $dir/build
 
-./write_grid2zn_str
-./read_grid2zn_str > output1
-diff <( sed '/Library/ d' output1) <( sed '/Library/ d' ../OUTPUT1) > results1.txt
-status=$?
-echoresults $status
-return_val=`expr $status + $return_val`
-
-./write_con2zn_genrl_str
-./read_con2zn_genrl_str > output2
-diff <( sed '/Library/ d' output2) <( sed '/Library/ d' ../OUTPUT2) > results2.txt
-status=$?
-echoresults $status
-return_val=`expr $status + $return_val`
+# loop through tests
+size_arr=${#w_arr[@]} #Number of elements in the array
+for i in $(seq 1 $size_arr);do
+   ./${w_arr[$i-1]}
+   ./${r_arr[$i-1]} > output$i
+   diff <( sed '/Library/ d' output$i) <( sed '/Library/ d' ../OUTPUT$i) > results$i.txt
+   status=$?
+   echoresults $status
+   return_val=`expr $status + $return_val`
+done
 
 cd ../..
 
