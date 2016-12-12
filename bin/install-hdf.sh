6a848459bfffd89be615c020c096d132cdace51f
M. Scot Breitenfeld <brtnfld@hdfgroup.org>
2016-12-12 14:26:26 -0600

    changed from svn to git for hdf5 repo.

--- bin/install-hdf.sh
+++ bin/install-hdf.sh
@@ -8,5 +8,5 @@ else
   export CC=gcc
 fi
 # 1.8 branch
-svn co -q http://svn.hdfgroup.uiuc.edu/hdf5/branches/hdf5_1_8
+git clone https://bitbucket.hdfgroup.org/scm/hdffv/hdf5.git --branch hdf5_1_8 --single-branch hdf5_1_8
 cd hdf5_1_8 && ./configure --disable-fortran --disable-hl --prefix=$HOME/hdf5 && make > result.txt 2>&1 && make install
