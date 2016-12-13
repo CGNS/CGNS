53b57f5c3f9ccd2b6d16ea1b0d228436eca463f6
Scot Breitenfeld <brtnfld@hdfgroup.org>
2016-12-12 22:41:30 -0600

    travis changes

--- bin/install-hdf.sh
+++ bin/install-hdf.sh
@@ -8,8 +8,5 @@ else
   export CC=gcc
 fi
 # 1.8 branch
-#git clone https://bitbucket.hdfgroup.org/scm/hdffv/hdf5.git --branch hdf5_1_8 --single-branch hdf5_1_8
-git clone https://bitbucket.hdfgroup.org/scm/hdffv/hdf5.git hdf5_1_8
-cd hdf5_1_8
-git checkout hdf5_1_8
+git clone https://bitbucket.hdfgroup.org/scm/hdffv/hdf5.git --branch hdf5_1_8 --single-branch hdf5_1_8
 ./configure --disable-fortran --disable-hl --prefix=$HOME/hdf5 && make > result.txt 2>&1 && make install
