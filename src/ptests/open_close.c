5a906954df44e5bca4bb89114020278b2de1438d
M. Scot Breitenfeld <brtnfld@hdfgroup.org>
2018-06-25 14:59:11 -0500

    added performance timing

--- src/ptests/open_close.c
+++ src/ptests/open_close.c
@@ -16,6 +16,9 @@
 #include "pcgnslib.h"
 #include "mpi.h"
 
+double xtiming[2], timing[2], timingMin[2], timingMax[2];
+double t1, t2;
+
 #define cgp_doError {printf("Error at %s:%u\n",__FILE__, __LINE__); return 1;}
 
 int main(int argc, char* argv[]) {
@@ -24,6 +27,7 @@ int main(int argc, char* argv[]) {
 	int comm_rank;
 	MPI_Info info;
 	int fn;
+	char fname[32];
 
 	err = MPI_Init(&argc,&argv);
 	if(err!=MPI_SUCCESS) cgp_doError;
@@ -44,10 +48,37 @@ int main(int argc, char* argv[]) {
 	if (cgp_close(fn))
 	    cgp_error_exit();
 #else
+	t1 = MPI_Wtime();
 	if (cgp_open("open_close.cgns", CG_MODE_WRITE, &fn))
 	    cgp_error_exit();
+	t2 = MPI_Wtime();
+	xtiming[0] = t2-t1;
+	t1 = MPI_Wtime();
 	if (cgp_close(fn))
 	    cgp_error_exit();
+	t2 = MPI_Wtime();
+	xtiming[1] = t2-t1;
+
+	MPI_Reduce(&xtiming, &timing, 2, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+	MPI_Reduce(&xtiming, &timingMin, 2, MPI_DOUBLE, MPI_MIN, 0, MPI_COMM_WORLD);
+	MPI_Reduce(&xtiming, &timingMax, 2, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+
+	if(comm_rank==0) {
+	  sprintf(fname, "OpenClose_time_%06d.dat", comm_size);
+	  FILE *fid = fopen(fname, "w");
+	  if (fid == NULL) {
+	    printf("Error opening timing file!\n");
+	  } else {
+	    fprintf(fid,"#nprocs, cgp_open, (min, max), cgp_close, (min, max)\n %d", comm_size);
+	    for ( int k = 0; k < 2; k++) {
+	      fprintf(fid," %.3f %.3f %.3f ",timing[k]/((double)comm_size), timingMin[k], timingMax[k]);
+	    }
+	    fprintf(fid,"\n");
+	    fclose(fid);
+	  }
+	}
+
+
 #endif
 	err = MPI_Finalize();
 	if(err!=MPI_SUCCESS) cgp_doError;
