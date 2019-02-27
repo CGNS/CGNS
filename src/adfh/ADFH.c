deb14c69f683e194fae3c144af1d3a53ad34b5d8
M. Scot Breitenfeld <brtnfld@hdfgroup.org>
2019-02-27 09:21:39 -0600

    Set the default HDF5 file format to 1.8.

--- src/adfh/ADFH.c
+++ src/adfh/ADFH.c
@@ -2058,10 +2058,15 @@ void ADFH_Database_Open(const char   *name,
   H5Pset_fclose_degree(g_propfileopen, H5F_CLOSE_STRONG);
 #endif
 
-  /*  H5Pset_latest_format(fapl, 1); */
-  /* Performance patch applied by KSH on 2009.05.18 */
+  /* Set to the latest, and allow backward compatibility, when CGNS
+     introduces newer HDF5 features. */
+
+  /*  H5Pset_libver_bounds(g_propfileopen,
+      H5F_LIBVER_LATEST, H5F_LIBVER_LATEST); */
+
   H5Pset_libver_bounds(g_propfileopen,
-		       H5F_LIBVER_LATEST, H5F_LIBVER_LATEST);
+		       H5F_LIBVER_V18, H5F_LIBVER_V18);
+
   /* open the file */
 
 #ifdef BUILD_PARALLEL
