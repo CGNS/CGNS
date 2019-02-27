c0692a3060a1371969e9a9e314e35a2b70499c8c
M. Scot Breitenfeld <brtnfld@hdfgroup.org>
2019-02-27 15:05:44 -0600

    added develop branch testing

--- bin/install-hdf.sh
+++ bin/install-hdf.sh
@@ -1,5 +1,24 @@
 #!/bin/sh
-# 1.8 branch
-git clone https://bitbucket.hdfgroup.org/scm/hdffv/hdf5.git --branch hdf5_1_8 --single-branch hdf5_1_8
-cd hdf5_1_8
+
+# Set odd/even days of the week
+day=$(( $(date +%d) % 2 ))
+
+if [ $day -eq 0 ]; then #even day tests, HDF5 1.8 branch
+
+HDF5_VER="hdf5_1_8"
+
+else #odd day tests, HDF5 develop branch
+
+HDF5_VER="develop"
+
+fi
+
+git clone https://bitbucket.hdfgroup.org/scm/hdffv/hdf5.git --branch $HDF5_VER --single-branch $HDF5_VER
+
+cd $HDF5_VER
+
+if [ ! -f configure ]; then
+  ./autogen.sh
+fi
+
 ./configure --disable-fortran --disable-hl --prefix=$HOME/hdf5 && make > result.txt 2>&1 && make install
