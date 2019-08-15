e1bc058739f4c8134be099bbacde523fc40becc3
M. Scot Breitenfeld <brtnfld@hdfgroup.org>
2019-08-15 16:21:59 -0500

    expanded open close test to include mixed serial and parallel open and close

--- src/ptests/open_close.c
+++ src/ptests/open_close.c
@@ -34,6 +34,44 @@ int main(int argc, char* argv[]) {
 	err = MPI_Info_create(&(info));
 	if(err!=MPI_SUCCESS) cgp_doError;
 
+        if(comm_rank == 0) {
+          if (cg_open("open_close.cgns", CG_MODE_WRITE, &fn))
+            cg_error_exit();
+
+          if (cg_close(fn))
+            cg_error_exit();
+        }
+
+        MPI_Barrier(MPI_COMM_WORLD);
+        if(cgp_open("open_close.cgns", CG_MODE_MODIFY, &fn))
+          cgp_error_exit();
+
+        if(cgp_close(fn))
+          cgp_error_exit();
+
+       if(cgp_open("open_close.cgns", CG_MODE_READ, &fn))
+          cgp_error_exit();
+
+        if(cgp_close(fn))
+          cgp_error_exit();
+
+        MPI_Barrier(MPI_COMM_WORLD);
+
+        cgp_mpi_comm(MPI_COMM_SELF);
+        // fails in 3.4.0
+        if(comm_rank == 0) {
+
+          if (cgp_open("open_close.cgns", CG_MODE_READ, &fn))
+            cg_error_exit();
+
+          if (cgp_close(fn))
+            cg_error_exit();
+
+        }
+
+        cgp_mpi_comm(MPI_COMM_WORLD);
+
+#if 1 //fails
 #ifdef DEBUG_MPI
         printf("[%d]cgp_open\n",comm_rank);
         fflush(stdout);
@@ -44,12 +82,15 @@ int main(int argc, char* argv[]) {
 	if (cgp_close(fn))
 	    cgp_error_exit();
 #else
+
 	if (cgp_open("open_close.cgns", CG_MODE_WRITE, &fn))
 	    cgp_error_exit();
 	if (cgp_close(fn))
 	    cgp_error_exit();
 #endif
+#endif
 	err = MPI_Finalize();
 	if(err!=MPI_SUCCESS) cgp_doError;
 	return err;
 	}
+
