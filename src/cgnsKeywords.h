b8a0a9c8a587b8853ea8470c95f48c02bbe4d809
M. Scot Breitenfeld <brtnfld@hdfgroup.org>
2020-02-19 16:15:49 -0600

    bumped to version 5.0.0

--- src/cgnsKeywords.h
+++ src/cgnsKeywords.h
@@ -63,8 +63,8 @@ freely, subject to the following restrictions:
  *      VERSION NUMBER                                                   *
 \* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
-#define CGNS_VERSION 4100
-#define CGNS_DOTVERS 4.10
+#define CGNS_VERSION 5000
+#define CGNS_DOTVERS 5.00
 #define CGNS_COMPATVERSION 2540
 #define CGNS_COMPATDOTVERS 2.54
 
