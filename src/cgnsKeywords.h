4568b4568ca996e14af834c91fc28d9b67d1c3a7
M. Scot Breitenfeld <brtnfld@hdfgroup.org>
2020-02-19 09:49:16 -0600

    bumped version to 4.1 for next release

--- src/cgnsKeywords.h
+++ src/cgnsKeywords.h
@@ -63,8 +63,8 @@ freely, subject to the following restrictions:
  *      VERSION NUMBER                                                   *
 \* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
-#define CGNS_VERSION 3130
-#define CGNS_DOTVERS 3.13
+#define CGNS_VERSION 4100
+#define CGNS_DOTVERS 4.10
 #define CGNS_COMPATVERSION 2540
 #define CGNS_COMPATDOTVERS 2.54
 
