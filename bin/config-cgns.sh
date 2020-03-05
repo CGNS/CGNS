2c5ce39a06a933ad627d3c0accd841942ec3514e
Scot Breitenfeld <brtnfld@hdfgroup.org>
2020-03-05 00:09:38 -0600

    added testing for cgnstools

--- bin/config-cgns.sh
+++ bin/config-cgns.sh
@@ -8,7 +8,9 @@ OPTS=""
 if [ $TRAVIS_OS_NAME = "linux" ]; then
   export FLIBS="-Wl,--no-as-needed -ldl"
   export LIBS="-Wl,--no-as-needed -ldl"
-  OPTS="--enable-parallel"
+  OPTS="--enable-parallel --enable-cgnstools --with-tcl=/usr/lib64 --with-tk=/usr/lib64"
+else
+  OPTS="--disable-cgnstools"
 fi
 
 ./configure \
@@ -19,7 +21,6 @@ fi
 --enable-64bit \
 --disable-shared \
 --enable-debug \
---disable-cgnstools \
 --enable-64bit
 
 
