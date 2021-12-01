32b89df945a0673fb859f1031115a13697895775
Ashcroft, Graham <graham.ashcroft@dlr.de>
2021-12-01 20:39:58 +0100

    Call cg_set_file_type in cgio_open_file ONLY if in write mode. Note, in all other cases the CGNS file type is determined by cgio_check_file.

--- src/cgns_io.c
+++ src/cgns_io.c
@@ -84,6 +84,8 @@ cgns_io_ctx_t ctx_cgio = { .hdf5_access = "NATIVE",
 /* Flag for contiguous or compact HDF5 storage */
 extern int HDF5storage_type;
 
+extern int cgns_filetype;
+
 typedef struct {
     int type;
     int mode;
@@ -750,7 +752,12 @@ int cgio_open_file (const char *filename, int file_mode,
         case 'w':
         case 'W':
             UNLINK(filename);
-            type = file_type;
+
+            /* set default file type if not done */
+            if (file_type == CG_FILE_NONE)
+                cg_set_file_type(CG_FILE_NONE);
+
+            type = file_type = cgns_filetype;
             file_mode = CGIO_MODE_WRITE;
             fmode = "NEW";
             break;
