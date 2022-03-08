e5e4ccc9daf0e796cb2dae22dc6f48fa7fad084d
Scot Breitenfeld <brtnfld@hdfgroup.org>
2022-03-08 15:46:33 -0600

    added m4 macros for opengl

--- src/cgnstools/common/gl_config.h
+++ src/cgnstools/common/gl_config.h
@@ -1,9 +1,27 @@
-#ifdef _WIN32
-#include <GL/gl.h>
-#include <GL/glu.h>
-#else
-#include <GL/gl.h>
-#include <GL/glu.h>
-#include <GL/glx.h>
+#ifndef CMAKE_BUILD
+#   include "CGNSconfig.h"
 #endif
-
+#if defined(HAVE_WINDOWS_H) && defined(_WIN32)
+#   include <windows.h>
+#endif
+#ifdef HAVE_GL_GL_H
+#   include <GL/gl.h>
+#elif defined(HAVE_OPENGL_GL_H)
+#   include <OpenGL/gl.h>
+#else
+#   error no gl.h
+#endif
+#ifdef HAVE_GL_GLU_H
+#   include <GL/glu.h>
+#elif defined(HAVE_OPENGL_GLU_H)
+#   include <OpenGL/glu.h>
+#else
+#   error no glu.h
+#endif
+#ifdef HAVE_GL_GLX_H
+#   include <GL/glx.h>
+#elif defined(HAVE_OPENGL_GLX_H)
+#   include <OpenGL/glx.h>
+#else
+#   error no glx.h
+#endif
