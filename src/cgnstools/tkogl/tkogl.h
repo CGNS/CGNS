d81283228f3951ace3f96827f12c487c9da96db2
carlos-pereyra <31297707+carlos-pereyra@users.noreply.github.com>
2023-07-11 13:05:56 -0700

    fix typo bug, and missing binaryio library ref.

--- src/cgnstools/tkogl/tkogl.h
+++ src/cgnstools/tkogl/tkogl.h
@@ -22,6 +22,8 @@
 #	        define EXPORT(a,b) a b
 #	    endif
 #   endif
+#elif defined(__APPLE__)
+#include <X11/Xatom.h>
 #else
 #   define EXPORT(a,b) a b
 #   include <X11/Xatom.h>		/* for XA_RGB_DEFAULT_MAP atom */
