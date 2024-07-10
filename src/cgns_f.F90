3422925716b51ab700f77af5804c996fce406958
Scot Breitenfeld <brtnfld@hdfgroup.org>
2024-07-10 12:09:07 -0500

    removed cg_nbases_f C wrapper call

--- src/cgns_f.F90
+++ src/cgns_f.F90
@@ -1102,13 +1102,6 @@ MODULE cgns
   !      Read and write CGNSBase_t Nodes
   ! - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-     SUBROUTINE cg_nbases_f(fn, nbases, ier) BIND(C, NAME="cg_nbases_f")
-       IMPLICIT NONE
-       INTEGER :: fn
-       INTEGER :: nbases
-       INTEGER, INTENT(OUT) :: ier
-     END SUBROUTINE cg_nbases_f
-
      SUBROUTINE cg_base_read_f(fn, B, basename, cell_dim, phys_dim, ier) !BIND(C, NAME="cg_base_read_f")
        USE ISO_C_BINDING
        IMPLICIT NONE
@@ -4597,6 +4590,35 @@ MODULE cgns
   PRIVATE cg_configure_ptr
 
 CONTAINS
+
+  ! - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  !      Read and write CGNSBase_t Nodes
+  ! - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+!DEC$if defined(BUILD_CGNS_DLL)
+!DEC$ATTRIBUTES DLLEXPORT :: cg_nbases_f
+!DEC$endif
+  SUBROUTINE cg_nbases_f(fn, nbases, ier)
+    IMPLICIT NONE
+    INTEGER, INTENT(IN)  :: fn
+    INTEGER, INTENT(OUT) :: nbases
+    INTEGER, INTENT(OUT) :: ier
+
+    INTEGER(C_INT) :: c_nbases
+
+    INTERFACE
+       INTEGER(C_INT) FUNCTION cg_nbases(fn, nbases) BIND(C, name="cg_nbases")
+         IMPORT ::C_INT
+         IMPLICIT NONE
+         INTEGER(C_INT), VALUE :: fn
+         INTEGER(C_INT) :: nbases
+       END FUNCTION cg_nbases
+    END INTERFACE
+
+    ier = INT(cg_nbases(INT(fn, C_INT), c_nbases))
+    nbases = INT(c_nbases)
+
+  END SUBROUTINE cg_nbases_f
 
 !DEC$if defined(BUILD_CGNS_DLL)
 !DEC$ATTRIBUTES DLLEXPORT :: cg_goto_f
