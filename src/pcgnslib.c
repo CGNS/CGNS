ecad30701941ac99a4d111c7f3f1ec73c1d4c582
Scot Breitenfeld <brtnfld@hdfgroup.org>
2024-12-05 10:21:11 -0600

    Doxygen updates

--- src/pcgnslib.c
+++ src/pcgnslib.c
@@ -1977,18 +1977,18 @@ int cgp_field_general_read_data(int fn, int B, int Z, int S, int F,
  *
  * \brief Create a coordinate data node by multiple processes in a parallel fashion.
  *
- * \param[in] fn \FILE_fn
- * \param[in] B \B_Base
- * \param[in] P \P_ParticleZone
- * \param[in] type \PGRID_datatype
+ * \param[in] fn        \FILE_fn
+ * \param[in] B         \B_Base
+ * \param[in] P         \P_ParticleZone
+ * \param[in] type      \PGRID_datatype
  * \param[in] coordname \PGRID_coordname
- * \param[out] C \PGRID_Coordinate
+ * \param[out] C        \PGRID_Coordinate
  * \return \ier
- * \details To write the data in parallel, first call \e cgp_coord_write to create an
- *          empty data node. This call is identical to \e cg_particle_coord_write with
+ * \details To write the data in parallel, first call cgp_coord_write() to create an
+ *          empty data node. This call is identical to cg_particle_coord_write() with
  *          \p coord_array set to NULL (no data written). The actual data is then written
- *          to the node in parallel using either \e cgp_particle_coord_write_data or
- *          \e cgp_particle_coord_general_write_data where \p range_min and \p range_max
+ *          to the node in parallel using either cgp_particle_coord_write_data() or
+ *          cgp_particle_coord_general_write_data() where \p range_min and \p range_max
  *          specify the subset of coordinate data to be written by a given process.
  */
 
@@ -2007,12 +2007,12 @@ int cgp_particle_coord_write(int fn, int B, int P, CGNS_ENUMT(DataType_t) type,
  *
  * \brief Write particle coordinate data in parallel.
  *
- * \param[in] fn \FILE_fn
- * \param[in] B \B_Base
- * \param[in] P \P_ParticleZone
- * \param[in] C \C_Coordinate
- * \param[in] rmin \PGRID_range_min
- * \param[in] rmax \PGRID_range_max
+ * \param[in] fn     \FILE_fn
+ * \param[in] B      \B_Base
+ * \param[in] P      \P_ParticleZone
+ * \param[in] C      \C_Coordinate
+ * \param[in] rmin   \PGRID_range_min
+ * \param[in] rmax   \PGRID_range_max
  * \param[in] coords \PGRID_coord_array
  * \return \ier
  *
@@ -2073,21 +2073,21 @@ int cgp_particle_coord_write_data(int fn, int B, int P, int C,
  *
  * \brief Write shaped array to a subset of grid coordinates in parallel.
  *
- * \param[in] fn \FILE_fn
- * \param[in] B \B_Base
- * \param[in] P \P_ParticleZone
- * \param[in] C \PGRID_Coordinate
- * \param[in] rmin \PGRID_range_min
- * \param[in] rmax \PGRID_range_max
- * \param[in] m_type \PGRID_mem_datatype
- * \param[in] m_numdim \PGRID_mem_rank
+ * \param[in] fn            \FILE_fn
+ * \param[in] B             \B_Base
+ * \param[in] P             \P_ParticleZone
+ * \param[in] C             \PGRID_Coordinate
+ * \param[in] rmin          \PGRID_range_min
+ * \param[in] rmax          \PGRID_range_max
+ * \param[in] m_type        \PGRID_mem_datatype
+ * \param[in] m_numdim      \PGRID_mem_rank
  * \param[in] m_arg_dimvals \PGRID_mem_dimensions
- * \param[in] m_rmin \PGRID_mem_range_min
- * \param[in] m_rmax \PGRID_mem_range_max
- * \param[out] coords \PGRID_coord_array
+ * \param[in] m_rmin        \PGRID_mem_range_min
+ * \param[in] m_rmax        \PGRID_mem_range_max
+ * \param[out] coords       \PGRID_coord_array
  * \return \ier
  *
- * \details The \e cgp_particle_coord_general_write_data perform data conversions
+ * \details The cgp_particle_coord_general_write_data() perform data conversions
  *          if \e datatype is different from \e mem_datatype. If \e coords == NULL, meaning
  *          this processor writes no data, then only \e fn, \e B, \e P, and \e C need be set.
  *          In this case, \e P and \e C are "representative" and can point to any valid zone.
@@ -2179,12 +2179,12 @@ int cgp_particle_coord_general_write_data(int fn, int B, int P, int C,
  *
  * \brief Read coordinate data in parallel.
  *
- * \param[in]  fn \FILE_fn
- * \param[in]  B \B_Base
- * \param[in]  P \P_ParticleZone
- * \param[in]  C \C_Coordinate
- * \param[in]  rmin \PGRID_range_min
- * \param[in]  rmax \PGRID_range_max
+ * \param[in]  fn     \FILE_fn
+ * \param[in]  B      \B_Base
+ * \param[in]  P      \P_ParticleZone
+ * \param[in]  C      \C_Coordinate
+ * \param[in]  rmin   \PGRID_range_min
+ * \param[in]  rmax   \PGRID_range_max
  * \param[out] coords \PGRID_coord_array
  * \return \ier
  *
@@ -2242,21 +2242,21 @@ int cgp_particle_coord_read_data(int fn, int B, int P, int C,
  *
  * \brief Read shaped array to a subset of grid coordinates in parallel.
  *
- * \param[in] fn \FILE_fn
- * \param[in] B \B_Base
- * \param[in] P \P_ParticleZone
- * \param[in] C \C_Coordinate
- * \param[in] rmin \PGRID_range_min
- * \param[in] rmax \PGRID_range_max
- * \param[in] m_type \PGRID_mem_datatype
- * \param[in] m_numdim \PGRID_mem_rank
+ * \param[in] fn            \FILE_fn
+ * \param[in] B             \B_Base
+ * \param[in] P             \P_ParticleZone
+ * \param[in] C             \C_Coordinate
+ * \param[in] rmin          \PGRID_range_min
+ * \param[in] rmax          \PGRID_range_max
+ * \param[in] m_type        \PGRID_mem_datatype
+ * \param[in] m_numdim      \PGRID_mem_rank
  * \param[in] m_arg_dimvals \PGRID_mem_dimensions
- * \param[in] m_rmin \PGRID_mem_range_min
- * \param[in] m_rmax \PGRID_mem_range_max
- * \param[out] coords \PGRID_coord_array
+ * \param[in] m_rmin        \PGRID_mem_range_min
+ * \param[in] m_rmax        \PGRID_mem_range_max
+ * \param[out] coords       \PGRID_coord_array
  * \return \ier
  *
- * \details The \e cgp_particle_coord_general_read_data perform data conversions if \e datatype
+ * \details The cgp_particle_coord_general_read_data() perform data conversions if \e datatype
  *          is different from \e mem_datatype. If \e coords == NULL, meaning
  *          this processor reads no data, then only \e fn, \e B, \e P, and \e C need be set.
  *          In this case, \e P and \e C are "representative" and can point to any valid zone.
@@ -2346,13 +2346,13 @@ int cgp_particle_coord_general_read_data(int fn, int B, int P, int C,
  *
  * \brief Create a particle solution field data node in parallel.
  *
- * \param[in] fn \FILE_fn
- * \param[in] B \B_Base
- * \param[in] P \P_ParticleZone
- * \param[in] S \PSOL_S
- * \param[in] DataType \PSOL_datatype
+ * \param[in] fn        \FILE_fn
+ * \param[in] B         \B_Base
+ * \param[in] P         \P_ParticleZone
+ * \param[in] S         \PSOL_S
+ * \param[in] DataType  \PSOL_datatype
  * \param[in] fieldname \PSOL_fieldname
- * \param[in] F \PSOL_F
+ * \param[in] F         \PSOL_F
  * \return \ier
  */
 int cgp_particle_field_write(int fn, int B, int P, int S,
@@ -2370,11 +2370,11 @@ int cgp_particle_field_write(int fn, int B, int P, int S,
  *
  * \brief Write field data in parallel.
  *
- * \param[in] fn \FILE_fn
- * \param[in] B \B_Base
- * \param[in] P \P_ParticleZone
- * \param[in] S \PSOL_S
- * \param[in] F \PSOL_F
+ * \param[in] fn   \FILE_fn
+ * \param[in] B    \B_Base
+ * \param[in] P    \P_ParticleZone
+ * \param[in] S    \PSOL_S
+ * \param[in] F    \PSOL_F
  * \param[in] rmin \PSOL_range_min
  * \param[in] rmax \PSOL_range_max
  * \param[in] data \PSOL_solution_array
@@ -2425,19 +2425,19 @@ int cgp_particle_field_write_data(int fn, int B, int P, int S, int F,
  *
  * \brief Write shaped array to a subset of particle solution field in parallel.
  *
- * \param[in] fn \FILE_fn
- * \param[in] B \B_Base
- * \param[in] P \P_ParticleZone
- * \param[in] S \PSOL_S
- * \param[in] F \PSOL_F
- * \param[in] rmin \PSOL_range_min
- * \param[in] rmax \PSOL_range_max
- * \param[in] m_type \PSOL_mem_datatype
- * \param[in] m_numdim \PSOL_mem_rank
+ * \param[in] fn            \FILE_fn
+ * \param[in] B             \B_Base
+ * \param[in] P             \P_ParticleZone
+ * \param[in] S             \PSOL_S
+ * \param[in] F             \PSOL_F
+ * \param[in] rmin          \PSOL_range_min
+ * \param[in] rmax          \PSOL_range_max
+ * \param[in] m_type        \PSOL_mem_datatype
+ * \param[in] m_numdim      \PSOL_mem_rank
  * \param[in] m_arg_dimvals \PSOL_mem_dimensions
- * \param[in] m_rmin \PSOL_mem_range_min
- * \param[in] m_rmax \PSOL_mem_range_max
- * \param[in] data \PSOL_solution_array
+ * \param[in] m_rmin        \PSOL_mem_range_min
+ * \param[in] m_rmax        \PSOL_mem_range_max
+ * \param[in] data          \PSOL_solution_array
  * \return \ier
  *
  * \details If \e data == NULL, meaning this processor reads no data, then
@@ -2530,11 +2530,11 @@ int cgp_particle_field_general_write_data(int fn, int B, int P, int S, int F,
  *
  * \brief Read particle field data in parallel.
  *
- * \param[in] fn \FILE_fn
- * \param[in] B \B_Base
- * \param[in] P \P_ParticleZone
- * \param[in] S \PSOL_S
- * \param[in] F \PSOL_F
+ * \param[in] fn   \FILE_fn
+ * \param[in] B    \B_Base
+ * \param[in] P    \P_ParticleZone
+ * \param[in] S    \PSOL_S
+ * \param[in] F    \PSOL_F
  * \param[in] rmin \PSOL_range_min
  * \param[in] rmax \PSOL_range_max
  * \param[in] data \PSOL_solution_array
@@ -2584,24 +2584,24 @@ int cgp_particle_field_read_data(int fn, int B, int P, int S, int F,
  *
  * \brief Read subset of particle solution field to a shaped array in parallel.
  *
- * \param[in] fn \FILE_fn
- * \param[in] B \B_Base
- * \param[in] P \P_ParticleZone
- * \param[in] S \PSOL_S
- * \param[in] F \PSOL_F
- * \param[in] rmin \PSOL_range_min
- * \param[in] rmax \PSOL_range_max
- * \param[in] m_type \PSOL_mem_datatype
- * \param[in] m_numdim \PSOL_mem_rank
+ * \param[in] fn            \FILE_fn
+ * \param[in] B             \B_Base
+ * \param[in] P             \P_ParticleZone
+ * \param[in] S             \PSOL_S
+ * \param[in] F             \PSOL_F
+ * \param[in] rmin          \PSOL_range_min
+ * \param[in] rmax          \PSOL_range_max
+ * \param[in] m_type        \PSOL_mem_datatype
+ * \param[in] m_numdim      \PSOL_mem_rank
  * \param[in] m_arg_dimvals \PSOL_mem_dimensions
- * \param[in] m_rmin \PSOL_mem_range_min
- * \param[in] m_rmax \PSOL_mem_range_max
- * \param[out] data \PSOL_solution_array
+ * \param[in] m_rmin        \PSOL_mem_range_min
+ * \param[in] m_rmax        \PSOL_mem_range_max
+ * \param[out] data         \PSOL_solution_array
  * \return \ier
  *
  * \details If \e data == NULL, meaning this processor reads no data, then
- *          only \e fn, \e B, \e P, \e S, and \e F need be set.  In this case, \e P, \e S, and \e F are
- *          "representative" and can point to any valid zone.
+ *          only \e fn, \e B, \e P, \e S, and \e F need be set.  In this case,
+ *          \e P, \e S, and \e F are "representative" and can point to any valid zone.
  */
 int cgp_particle_field_general_read_data(int fn, int B, int P, int S, int F,
                                          const cgsize_t *rmin, const cgsize_t *rmax,
@@ -3011,9 +3011,10 @@ int cgp_array_general_read_data(int A,
   Multidataset APIs
 *********************************/
 
-static int readwrite_multi_data_parallel(size_t count, hid_t *dset_id, hid_t *mem_type_id, hid_t *mem_space_id, hid_t *file_space_id,
-                                         cg_rw_ptr_t *data,
-					 int ndims, const cgsize_t *rmin, const cgsize_t *rmax, enum cg_par_rw rw_mode)
+static int readwrite_multi_data_parallel(size_t count, hid_t *dset_id, hid_t *mem_type_id,
+                                         hid_t *mem_space_id, hid_t *file_space_id,
+                                         cg_rw_ptr_t *data, int ndims, const cgsize_t *rmin,
+                                         const cgsize_t *rmax, enum cg_par_rw rw_mode)
 {
   /*
    *  Needs to handle a NULL dataset. MSB
