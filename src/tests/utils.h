729af3bdfd009ddab866b9440dafc406dfa2314b
Mickael PHILIT <mickey.phy@gmail.com>
2025-10-15 12:27:13 +0200

    use parenthesis

--- src/tests/utils.h
+++ src/tests/utils.h
@@ -98,8 +98,8 @@ int write_test_header(char *title_header, int len)
 
   width = TAB_SPACE+10;
 
-  char *title_centered = (char*)malloc(4*width+1*sizeof(char));
-  char *str = (char*)malloc(2*width+2*sizeof(char));
+  char *title_centered = (char*)malloc((4*width+1)*sizeof(char));
+  char *str = (char*)malloc((2*width+2)*sizeof(char));
 
   memcpy(str,title_header,len);
   str[len] = '\0';
